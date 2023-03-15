328d442d9c373fe7acb1f96fd2823a6770a356c0
alexgmz.dev <125255777+alexgmzdev@users.noreply.github.com>
2023-03-15 20:16:35 +0100

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,24 @@
+package ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number1=1;
+		long number2=345;
+		double number3=24.5;
+		boolean check=true;
+		String nombre="ALEX";
+		
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(check);
+		System.out.println(nombre);
+		
+		
+	}
+
+}
